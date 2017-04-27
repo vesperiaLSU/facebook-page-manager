7e91c2df2d3192d21b000dc5a06c655f8d1ed686
Chen Guo <guochen1223@gmail.com>
2017-04-27 17:13:52 +0000

    config update

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@
         facebookAuth: {
             clientID: '1446253795418031',
             clientSecret: '1e4ff780b141ee71ab23142a858794a9',
-            callBackUrl: 'http://facebookpm.herokuapp.com/auth/facebook/callback'
+            callBackUrl: 'https://facebookpm.herokuapp.com/auth/facebook/callback'
         },
         facebookAPI: {
             getPageAccessToken: 'https://graph.facebook.com/v2.9/1474589055913389?fields=access_token',
